391ff1cabe612f253d65f23ca7c59ce37a588bfa
bijay135 <bijay.shah@sastodeal.com>
2020-01-24 11:28:52 +0545

    renamed theme

--- app/etc/config.php
+++ app/etc/config.php
@@ -237,6 +237,7 @@ return [
         'Amazon_Core' => 1,
         'Amazon_Login' => 1,
         'Amazon_Payment' => 1,
+        'Bijay_CustomCategoryAttribute' => 1,
         'Dotdigitalgroup_Email' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
