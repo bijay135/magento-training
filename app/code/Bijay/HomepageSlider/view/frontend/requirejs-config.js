a8af11837f3bdbb86a74fe554b8b5dfd7c7c7e80
bijay135 <bijay.shah@sastodeal.com>
2020-01-28 16:13:32 +0545

    owl.caurosel basic implementation

---
+++ app/code/Bijay/HomepageSlider/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+    "map": {
+        "*": {
+            "OwlCarousel": "Bijay_HomepageSlider/js/owl-carousel"
+        }
+    },
+    "shim":{
+        "Bijay_HomepageSlider/js/owl.carousel.min": ["jquery"]
+    }
+};
